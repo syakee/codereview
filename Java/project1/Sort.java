e20d87c12c01cc9ec8c868ff01a756a3f2611cef
syakee <36218302+syakee@users.noreply.github.com>
2019-04-17 10:18:07 +0900

    Update Sort.java

--- Java/project1/Sort.java
+++ Java/project1/Sort.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.lang.*;
+
 class Sort
 {
 	public static void main(String args[])throws IOException
@@ -9,16 +10,21 @@ class Sort
 		do
 	{
 		System.out.println("\n\n\n1.Bubble Sort\n2.Selection Sort\n3.Insertion Sort.\n4.Quick Sort.\n5.Merge Sort.\n6.Exit.");
-		 ch=Integer.parseInt(br.readLine());
+		ch=Integer.parseInt(br.readLine());
+			
 		if(ch==6)
 			return;
+			
 		System.out.println("Enter n");
+			
 		int n=Integer.parseInt(br.readLine());
 		int a[]=new int[n];
+			
 		for(int i=0;i<n;i++)
 		{
 			a[i]=Integer.parseInt(br.readLine());
 		}
+			
 		switch(ch)
 		{
 			case 1:
@@ -43,8 +49,10 @@ class Sort
 		}
 		
 	}
+		
 	while(ch!=6);
 	}
+	
 	public static void BinarySort(int a[],int n)
 	{
 		int temp;
@@ -65,6 +73,7 @@ class Sort
 		}
 		print(a,n);
 	}
+	
 	public static void SelectionSort(int a[], int n)
 	{
 		for (int i=0;i<n-1;i++)
@@ -82,6 +91,7 @@ class Sort
 		}
 		print(a,n);
 	}
+	
 	public static void InsertionSort(int a[],int n)
 	{
 		for(int i=1;i<n;i++)
@@ -97,9 +107,9 @@ class Sort
 		}
 		print(a,n);
 	}
+	
 	public static void MergeSort(int a[],int n)
 	{
-		
 		if(n<=1)
 			return;
 		int mid=n/2;
@@ -114,6 +124,7 @@ class Sort
 		Merge(left,right,a);
 		
 	}
+	
 	public static void Merge(int left[],int right[],int a[])
 	{
 		int nL=left.length;
@@ -148,6 +159,7 @@ class Sort
 			k++;
 		}
 	}
+	
 	public static void QuickSort(int a[],int start,int end)
 	{
 		if(start<end)
@@ -160,6 +172,7 @@ class Sort
 			return;
 		
 	}
+	
 	public static int QuickPartition(int a[],int start,int end)
 	{
 		int temp;
@@ -170,17 +183,18 @@ class Sort
 			if(a[i]<=pivot)
 			{
 				//swap a[i],apindex
-					temp=a[i];
-					a[i]=a[pIndex];
-					a[pIndex]=temp;
-					pIndex++;
+				temp=a[i];
+				a[i]=a[pIndex];
+				a[pIndex]=temp;
+				pIndex++;
 			}
 		}
-					temp=a[pIndex];
-					a[pIndex]=a[end];
-					a[end]=temp;
-					return pIndex;
+		temp=a[pIndex];
+		a[pIndex]=a[end];
+		a[end]=temp;
+		return pIndex;
 	}
+	
 	public static void print(int a[],int n)
 	{
 		System.out.println();
